9d14c1c5174720083647417c246efaa35a591411
Yueying <wangyy395@gmail.com>
2021-04-22 12:09:59 -0400

    add item constuctor

--- src/Item.java
+++ src/Item.java
@@ -1,4 +1,17 @@
 public class Item {
 
     // name, wholesalePrice, remaining, isOnSale, price
+    public String name;
+    public double wholesalePrice;
+    public int remaining;
+    public boolean isOnsale;
+    public double price;
+
+    public Item(String newName, double wholesalePrice, int remaining, double price) {
+        this.name = newName;
+        this.wholesalePrice = wholesalePrice;
+        this.remaining = remaining;
+        this.price = price;
+        this.isOnsale = false;
+    }
 }
